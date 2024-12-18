260f053c22c9dd0d733e2f61e41fda9acf1dcf56
SinPhosis <sionclasher@gmail.com>
2024-12-18 17:29:31 +0800

    board

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// 
+// board section
+const createConnect4Board = () => {
+const gameBoard = document.getElementById("container")
+
+const board = document.createElement("div");
+board.classList.add("board");
+document.body.appendChild(board)
+
+for (let i = 0; i < 42; i++) {
+    const slot = document.createElement('div');
+    slot.classList.add("slot");
+    board.appendChild(slot);
+}    
+};
+createConnect4Board();
